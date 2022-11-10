49f7614d9c423959071c89249e218d3bd3231c0e
CIDRA <cidra3204@gmail.com>
2022-11-10 20:17:08 +0900

    add: トークンの自動更新を実装

--- app/src/main/java/com/example/twitterapptest/MainActivity.kt
+++ app/src/main/java/com/example/twitterapptest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.twitterapptest
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -8,6 +9,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -15,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.example.twitterapptest.ui.theme.TwitterAppTestTheme
@@ -22,7 +25,6 @@ import com.github.scribejava.core.model.OAuth2AccessToken
 import com.github.scribejava.core.pkce.PKCE
 import com.github.scribejava.core.pkce.PKCECodeChallengeMethod
 import com.github.scribejava.core.revoke.TokenTypeHint
-import com.twitter.clientlib.ApiClientCallback
 import com.twitter.clientlib.TwitterCredentialsOAuth2
 import com.twitter.clientlib.api.TwitterApi
 import com.twitter.clientlib.auth.TwitterOAuth20Service
@@ -38,7 +40,11 @@ class MainActivity : ComponentActivity() {
         const val SECRET_STATE = "state"
         const val TWITTER_OAUTH2_CLIENT_ID = "Developer Portalで取得したCLIENT ID"
         const val TWITTER_OAUTH2_CLIENT_SECRET = "Developer Portalで取得したCLIENT SECRET ID"
+        // 1時間半でトークンを更新(トークンの有効期限は2時間だが余裕を見るため)
+        const val EXPIRY_TIME = 5400000
     }
+
+    private lateinit var prefs: SharedPreferences
 
     private lateinit var credentials: TwitterCredentialsOAuth2
     private lateinit var service: TwitterOAuth20Service
@@ -47,16 +53,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val prefs = getSharedPreferences("TwitterOauth", MODE_PRIVATE)
+        prefs = getSharedPreferences("TwitterOauth", MODE_PRIVATE)
 
         credentials = TwitterCredentialsOAuth2(
             TWITTER_OAUTH2_CLIENT_ID,
             TWITTER_OAUTH2_CLIENT_SECRET,
             prefs.getString("OauthToken", ""),
             prefs.getString("OauthRefreshToken", "")
-        ).apply {
-            isOAUth2AutoRefreshToken = true // トークンの自動更新有効化
-        }
+        )
 
         service = TwitterOAuth20Service(
             credentials.twitterOauth2ClientId,
@@ -105,13 +109,29 @@ class MainActivity : ComponentActivity() {
                         Button(
                             onClick = {
                                 scope.launch(Dispatchers.IO) {
+                                    refreshToken()
+                                }
+                            }
+                        ) {
+                            Text(text = "トークンの更新")
+                        }
+                        
+                    }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Button(
+                            onClick = {
+                                scope.launch(Dispatchers.IO) {
                                     getBookmark()
                                 }
                             }
                         ) {
                             Text(text = "ブックマーク取得")
                         }
-
                     }
                 }
             }
@@ -128,24 +148,45 @@ class MainActivity : ComponentActivity() {
         Log.i("onNewIntent", "onNewIntent")
 
         val uri = intent?.data
-        val code = uri!!.getQueryParameter("code")
+        val code = uri?.getQueryParameter("code")
 
+        // 認可コードが正常に取得できていたらトークンを取得
         lifecycleScope.launch(Dispatchers.IO) {
-            code?.let {
-                getAccessToken(it)
+            if(code != null) {
+                getAccessToken(code)
             }
         }
 
     }
 
     /**
+     * 初回起動時、バックグラウンドから復帰時に
+     * 現在時間とトークン取得時間を比較し
+     * 有効期限が切れていたらトークンを更新する
+     */
+    override fun onResume() {
+        super.onResume()
+
+        val getTokenTime = prefs.getLong("getTokenTime", 0)
+        val now = System.currentTimeMillis()
+        val elapsedTime = now - getTokenTime
+
+        if (elapsedTime > EXPIRY_TIME) {
+            lifecycleScope.launch(Dispatchers.IO) {
+                refreshToken()
+            }
+        }
+    }
+
+    /**
      * アクセストークンの取得
      */
-    private suspend fun getAccessToken(code: String?) {
+    private suspend fun getAccessToken(code: String) {
 
         val accessToken = service.getAccessToken(pkce, code)
+        val getTokenTime = System.currentTimeMillis()
 
-        storeToken(accessToken)
+        storeToken(accessToken, getTokenTime)
 
         withContext(Dispatchers.Main) {
             val toast = Toast.makeText(applicationContext, "認証完了", Toast.LENGTH_SHORT)
@@ -159,12 +200,13 @@ class MainActivity : ComponentActivity() {
     /**
      * アクセストークンの保管
      */
-    private fun storeToken(accessToken: OAuth2AccessToken) {
+    private fun storeToken(accessToken: OAuth2AccessToken, getTokenTime: Long) {
 
-        val prefs = getSharedPreferences("TwitterOauth", MODE_PRIVATE)
+//        val prefs = getSharedPreferences("TwitterOauth", MODE_PRIVATE)
         prefs.edit().apply {
             putString("OauthToken", accessToken.accessToken)
             putString("OauthRefreshToken", accessToken.refreshToken)
+            putLong("getTokenTime", getTokenTime)
         }.apply()
 
         credentials.apply {
@@ -224,7 +266,7 @@ class MainActivity : ComponentActivity() {
         Log.i("getBookMark", credentials.twitterOauth2RefreshToken)
 
         runCatching {
-            apiInstance.bookmarks().getUsersIdBookmarks("2942060174").execute()
+            apiInstance.bookmarks().getUsersIdBookmarks("認証したユーザーのIDを入力").execute()
         }.onSuccess {
             withContext(Dispatchers.Main) {
                 val toast = Toast.makeText(applicationContext, "ブックマーク取得成功！", Toast.LENGTH_SHORT)
@@ -250,7 +292,7 @@ class MainActivity : ComponentActivity() {
 
         service.revokeToken(credentials.twitterOauth2AccessToken, TokenTypeHint.ACCESS_TOKEN)
 
-        val prefs = getSharedPreferences("TwitterOauth", MODE_PRIVATE)
+
         prefs.edit().apply {
             putString("OauthToken", "")
             putString("OauthRefreshToken", "")
@@ -266,5 +308,43 @@ class MainActivity : ComponentActivity() {
             toast.show()
         }
 
+    }
+
+    /**
+     * トークンの更新
+     */
+    private suspend fun refreshToken() {
+
+        val apiInstance = TwitterApi(credentials)
+
+        apiInstance.addCallback {
+            credentials.apply {
+                twitterOauth2AccessToken = it.accessToken
+                twitterOauth2RefreshToken = it.refreshToken
+            }
+
+            val refreshTokenTime = System.currentTimeMillis()
+
+            prefs.edit().apply {
+                putString("OauthToken", it.accessToken)
+                putString("OauthRefreshToken", it.refreshToken)
+                putLong("getTokenTime", refreshTokenTime)
+            }.apply()
+
+        }
+
+        runCatching {
+            apiInstance.refreshToken()
+            withContext(Dispatchers.Main) {
+                val toast = Toast.makeText(applicationContext, "トークン更新成功！", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }.getOrElse {
+            withContext(Dispatchers.Main) {
+                val toast = Toast.makeText(applicationContext, "トークン更新失敗", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+            Log.i("refreshToken_Fail", "$it")
+        }
     }
 }
